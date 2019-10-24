e409289f32b7a91ea892f5ab6e72d25d54527a8d
weiyajun2688 <weiyajun2688@ipalfish.com>
2019-10-24 23:19:12 +0800

    引入less

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
     <div id='root'>
-        <el-tabs tab-position="'left'"
-            style="height: 200px;">
+        <el-tabs tab-position="left"
+            style="height: 100%">
             <el-tab-pane label="放松一下">用户管理</el-tab-pane>
-            <el-tab-pane label="开发辅助">配置管理</el-tab-pane>
+            <el-tab-pane label="开发辅助">
+                开发辅助
+                <assist></assist>
+            </el-tab-pane>
             <el-tab-pane label="减压游戏">角色管理</el-tab-pane>
         </el-tabs>
     </div>
 </template>
 <script>
-
+import Assist from '@/views/Assist.vue'
 export default {
+  components: {
+    Assist
+  },
   data() {
     return {
     };
   }
 };
 </script>
+<style lang='less'>
+html,
+body,
+#app,
+#root {
+    height: 100%;
+}
+</style>
---
+++ src/views/Assist.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="assit"
+        flex="dir:top box:mean">
+        <div>常见API</div>
+        <div>辅助工具</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Assit',
+  props: {
+  }
+}
+</script>
+<style scoped>
+#assit {
+    height: 100%;
+}
+</style>
